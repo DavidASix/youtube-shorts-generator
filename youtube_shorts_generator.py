f8de6cd7856236f222374f92c6969b43625bfa0e
davidasix <davidandersonsix@gmail.com>
2023-05-26 00:02:08 -0400

    Adjusted length printout for fandom library bug

--- youtube_shorts_generator.py
+++ youtube_shorts_generator.py
@@ -14,12 +14,17 @@ def main():
         # 2000 characters seems like a good minimum
         try:
             page = fandom.page(p[0])
-            print(page)
-            print('---Page:', page.title, 'total length', len(page.plain_text))
+            # Some pages have the characters "v · d · e" for View Template, Discussion, Edit
+            # The text after this delimiter can be very long with many links to other semi-related articles
+            # Unfortuantely this text is captured in whatever final section appears on the page, after the scrape
+            length = len(page.plain_text.split('v · d · e')[0])
+            print('---Page:', page.title)
+            print('Actual length', length)
             print(page.url)
+
             s = page.sections
             for section in s:
-                print(section, 'length', len(page.section(section)))
+                print(section, 'length', len(page.section(section).split('v · d · e')[0]))
         except Exception as e:
             print(e)
         print('\n')
