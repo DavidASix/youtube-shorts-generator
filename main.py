6478aa45e7233f535efba16a8b7fd5bdc17ef90d
davidasix <davidandersonsix@gmail.com>
2023-06-22 13:44:34 -0400

    Testing media_wiki class

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import manually_classify_pages
-import train_algorithm
+#import train_algorithm
+import common.media_wiki as media_wiki
 
 def main():
-    manually_classify_pages.main()
+    #manually_classify_pages.main()
     #train_algorithm.main()
+    media_wiki
+    
 
 if __name__ == "__main__":
     main()
