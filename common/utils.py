9f2ce3dc669105b2081b6f193eb7e1e99e3b4b56
davidasix <davidandersonsix@gmail.com>
2023-06-22 13:44:19 -0400

    Created utils file to hold utility functions used across Classes

---
+++ common/utils.py
@@ -0,0 +1,14 @@
+def get_curly_content(content):
+    # receives a string like "{my content {} {} {{nested curlies}} end content}"
+    # Will return whatever string is between the opening and closing curlies
+    string = content.strip()
+    open_brackets = 0
+    end_pos = 0
+    for i, char in enumerate(string):
+        open_brackets += 1 if char == '{' else 0
+        open_brackets -= 1 if char == '}' else 0
+        if open_brackets == 0:
+            end_pos = i
+            break
+    print(string)
+    return string[1:end_pos]
