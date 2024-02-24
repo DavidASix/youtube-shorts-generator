814ef25817b6b0f0b5bea62151123664ad577405
David Anderson <davidandersonsix@gmail.com>
2024-02-24 17:38:28 -0500

    Converted to SQLITE and added feature metric

--- train_algorithm.py
+++ train_algorithm.py
@@ -2,33 +2,40 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
-from scipy.stats import randint
+from sklearn.preprocessing import MinMaxScaler
+import sqlite3
+import matplotlib.pyplot as plt
 
-from common.mysql import sql
+def test_model():
+    conn = sqlite3.connect('ytsg-dataset.db')
+    query = """SELECT 
+        * ,
+        (LENGTH(categories) - LENGTH(REPLACE(categories, "||", "|")) +1) AS category_count 
+    FROM manual_page_classifications LIMIT 5000"""
+    df = pd.read_sql_query(query, conn)
 
-
-def get_sample_df():
-    db_engine = sql()
-    query = 'SELECT * FROM manual_page_classifications LIMIT 5000'
-    df = pd.read_sql_query(query, db_engine.connection)
-    print(df)
     feature_columns = [
         'total_length',
-        'non_english',
         'file_page',
-        'short_page',
-        'first_section_length',
         'target_words_in_section_titles',
+        'category_count',
         'section_count',
         'image_count',
         'audio_count'
     ]
+    
+    # Normalize values
+    # scaler = MinMaxScaler()
+    # for column in feature_columns:
+    #     df[column] = scaler.fit_transform(df[[column]])
+    # print(df[feature_columns].head(5))
+
     # Split up features (x) and target (y)
     x = df[feature_columns]
     y = df['rating_class']
 
     # Split up training and test sets
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=11)
 
     # Train the model
     print('Training Model')
@@ -43,6 +50,14 @@ def get_sample_df():
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
 
+    # print('Confusion Matrix:\n', confusion_matrix(y_test, y_pred))
+    # disp = ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred), display_labels=rf.classes_)
+    # disp.plot()
+    # plt.savefig('confusion_matrix.png')
+
 
 def main():
-    get_sample_df()
+    test_model()
+
+if __name__ == '__main__':
+    main()
